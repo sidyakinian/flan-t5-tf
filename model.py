33af5a56214bddba8fc7425a176d25c4160eaa1b
Yan Sidyakin <sidyakinian@gmail.com>
2022-11-13 18:54:23 -0800

    Add self-attention operation

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 from json import decoder, encoder
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, LayerNormalization, Dropout, Activation, GlobalAveragePooling1D
+from tensorflow.keras.layers import Dense, LayerNormalization, Dropout, Activation, Layer
 
-class EmbeddingsLayer(tf.keras.layers.Layer):
+class EmbeddingsLayer(Layer):
     def __init__(self, vocab_size: int, d_model: int):
         super().__init__()
         self.vocab_size = vocab_size
@@ -23,3 +23,15 @@ class MLP(Layer):
         x = self.fc1(x)
         x = self.fc2(x)
         return x
+
+class SelfAttention(Layer):
+    def __init__(self, d_k: int):
+        super().__init__()
+        self.d_k = d_k
+
+    def call(self, K: tf.Tensor, Q: tf.Tensor, V: tf.Tensor) -> tf.Tensor:
+        # K, Q, V are all of shape (batch_size, n_heads, n, d_k)
+        K = tf.transpose(K, perm=[0, 1, 3, 2]) # (batch_size, n_heads, d_k, n)
+        QK = tf.matmul(Q, K) / tf.math.sqrt(tf.cast(self.d_k, dtype=tf.float32)) # shape (batch_size, n_heads, n, n)
+        P_bar = tf.nn.softmax(QK, axis=-1)
+        return tf.matmul(P_bar, V) # (batch_size, n_heads, n, d_k)
