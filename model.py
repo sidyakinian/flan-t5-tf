5785ee45a3a758b4449b3f46d9d1fc14762e1cfa
Yan Sidyakin <sidyakinian@gmail.com>
2022-11-13 18:54:54 -0800

    Add multi-head attention layer

--- model.py
+++ model.py
@@ -35,3 +35,40 @@ class SelfAttention(Layer):
         QK = tf.matmul(Q, K) / tf.math.sqrt(tf.cast(self.d_k, dtype=tf.float32)) # shape (batch_size, n_heads, n, n)
         P_bar = tf.nn.softmax(QK, axis=-1)
         return tf.matmul(P_bar, V) # (batch_size, n_heads, n, d_k)
+
+def reshape_for_multihead_attention(M, batch_size, n, n_heads, d_k):
+    M = tf.reshape(M, shape=[batch_size, n, n_heads, d_k])
+    M = tf.transpose(M, perm=[0, 2, 1, 3]) # shape (batch_size, n_heads, n, d_k)
+    return M
+
+class MultiHeadSelfAttention(Layer):
+    def __init__(self, d_k: int, d_model: int, n_heads: int):
+        super().__init__()
+        assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
+        self.d_model = d_model
+        self.d_k = d_k
+        self.n_heads = n_heads
+        self.self_attention = SelfAttention(d_k=self.d_k)
+        self.w_k = Dense(self.d_k * n_heads) # equals d_model but more illustrative
+        self.w_q = Dense(self.d_k * n_heads)
+        self.w_v = Dense(self.d_k * n_heads)
+        self.w_o = Dense(self.d_model)
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        # shape of K, Q, V: (batch_size, n, d_model)
+        assert tf.shape(K) == tf.shape(Q) == tf.shape(V), "K, Q, V must have the same shape" # Idk if tf.shape will work with XLA
+        # TODO: find another way to determine shape other than passing batch size on model init
+        batch_size, n, d_k = tf.shape(K).numpy()
+        K, Q, V = self.w_k(x), self.w_q(x), self.w_v(x)
+
+        K = reshape_for_multihead_attention(K, batch_size, n, self.n_heads, self.d_k)  
+        Q = reshape_for_multihead_attention(Q, batch_size, n, self.n_heads, self.d_k)
+        V = reshape_for_multihead_attention(V, batch_size, n, self.n_heads, self.d_k)
+        
+        attention_output = self.self_attention(K, Q, V)
+        # Concat heads with transpose and reshape
+        attention_output = tf.transpose(attention_output, perm=[0, 2, 1, 3]) # shape (batch_size, n, n_heads, d_k)
+        attention_output = tf.reshape(attention_output, shape=[batch_size, n, self.d_model]) # shape (batch_size, n, d_model)
+        outputs = self.w_o(attention_output)
+
+        return outputs
